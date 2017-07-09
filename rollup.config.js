4818369c4c23b4d16983e49fe43f668b3d810aec
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-07-09 23:24:23 +1000

    feat(plugin): inline css and build fixes

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,34 @@
+import postcss from 'rollup-plugin-postcss';
+import postcssModules from 'postcss-modules';
+
+const cssExportMap = {};
+
 export default {
-  format: 'umd',
-  moduleName: 'plugin',
-  onwarn: ( warning ) => {
-      const skip_codes = [
-          'THIS_IS_UNDEFINED',
-          'MISSING_GLOBAL_NAME'
-      ];
-      if ( skip_codes.indexOf(warning.code) != -1 ) return;
-      console.error(warning);
-  }
+    entry: 'dist/index.js',
+    dest: 'dist/index.bundle.js',
+    format: 'umd',
+    moduleName: 'plugin',
+    external: ['aurelia-pal', 'aurelia-framework'],
+    plugins: [
+        postcss({
+            plugins: [
+                postcssModules({
+                    getJSON(id, exportTokens) {
+                        cssExportMap[id] = exportTokens;
+                    }
+                })
+            ],
+            getExport(id) {
+                return cssExportMap[id];
+            }
+        })
+    ],
+    onwarn: (warning) => {
+        const skip_codes = [
+            'THIS_IS_UNDEFINED',
+            'MISSING_GLOBAL_NAME'
+        ];
+        if (skip_codes.indexOf(warning.code) != -1) return;
+        console.error(warning);
+    }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import "./styles.css"
-
+import './styles.css';
 import {FrameworkConfiguration} from 'aurelia-framework';
+
 export function configure(aurelia: FrameworkConfiguration) {
     aurelia.globalResources([
 
